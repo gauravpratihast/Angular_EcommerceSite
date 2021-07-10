3d22364c6747a93fa098fac2530100ce8a2636bb
Gaurav Pratihast <pratihastguarav@gmail.com>
2021-07-10 10:03:52 +0530

    deploying

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/new-website'));
+app.use(express.static(__dirname + '/dist/newWebsite'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/new-website/index.html'));
+res.sendFile(path.join(__dirname+'/dist/newWebsite/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
